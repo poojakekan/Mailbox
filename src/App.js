bdc03dac976cf696291b03216ccde6e08ea8ebd1
Pooja Kekan <35685820+poojakekan@users.noreply.github.com>
2024-08-05 22:03:52 +0530

    Added firebase

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import './App.css';
-import Navbar from './components/Navbar';
-import Signup from './components/Signup';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Header from './components/Header';
+import SignupForm from './components/SignupForm';
+import LogInForm from './components/LogInForm';
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Signup/>
+      
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/signup" element={<SignupForm />} />
+        <Route path="/login" element={<LogInForm />} />
+      </Routes>
+    </BrowserRouter>
     </div>
   );
 }
+
 export default App;
